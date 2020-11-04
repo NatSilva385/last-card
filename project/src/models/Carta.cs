0d06afa9e11355e14d74a83087bfce3c105cc0ac
NathaliaQuetsia <skywalkercomplex@gmail.com>
2020-11-04 09:42:36 -0300

    Inicializando o repositório

---
+++ project/src/models/Carta.cs
@@ -0,0 +1,24 @@
+namespace project.src.models
+{
+    public class Carta
+    {
+
+    }
+
+    public enum COR
+    {
+        SEMCOR,
+        AZUL,
+        VERMELHO,
+        VERDE,
+        AMARELO
+    }
+
+    public enum SHADER_CARTA_FRENTE
+    {
+        PADRAO,
+        METALICO,
+        CRISTAL
+    }
+
+}
---
+++ project/src/models/CartaCor.cs
@@ -0,0 +1,18 @@
+
+namespace project.src.models
+{
+    public class CartaCor
+    {
+        public long CorShader
+        {
+            get;
+            set;
+        }
+
+        public COR CorCarta
+        {
+            get;
+            set;
+        }
+    }
+}
